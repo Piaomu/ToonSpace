b1029b47b265b3914aa71c9e8f13d804584ad0d3
piaomu <driftwood.wahl@gmail.com>
2021-05-12 09:15:09 -0500

    Added IImageService and BasicImageService

---
+++ ToonSpace/Services/BasicImageService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ToonSpace.Services
+{
+    public class BasicImageService
+    {
+        public string DecodeImage(byte[] image, string contentType)
+        {
+            if (image == null)
+            {
+                return null;
+            }
+            var convertedImage = Convert.ToBase64String(image);
+            return $"data:{contentType};base64,{convertedImage}";
+        }
+
+        public async Task<byte[]> EncodeImageURLAsync(string imageURL)
+        {
+            var client = new HttpClient();
+
+            var response = await client.GetAsync(imageURL);
+
+            Stream stream = await response.Content.ReadAsStreamAsync();
+
+            var ms = new MemoryStream();
+            await stream.CopyToAsync(ms);
+
+            return ms.ToArray();
+
+        }
+
+        public async Task<byte[]> EncodeImageAsync(IFormFile image)
+        {
+            if (image == null)
+            {
+                return null;
+            }
+            using var ms = new MemoryStream();
+            await image.CopyToAsync(ms);
+            return ms.ToArray();
+
+        }
+
+        public string RecordContentType(IFormFile image)
+        {
+            if (image == null)
+            {
+                return null;
+            }
+            return image.ContentType;
+        }
+    }
+}
---
+++ ToonSpace/Services/Interface.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToonSpace.Services
+{
+    public interface IImageService
+    {
+        Task<byte[]> EncodeImageAsync(IFormFile poster);
+
+        //encode an image from a url
+        Task<byte[]> EncodeImageURLAsync(string imageURL);
+
+        string DecodeImage(byte[] poster, string contentType);
+
+        string ContentType(IFormFile poster);
+    }
+}
