1ceb069d39926ccbc24710b167d9b5840bd4c2c1
piaomu <driftwood.wahl@gmail.com>
2021-07-24 21:26:08 -0500

    Define Details Page and Add ArtistPageViewModel

---
+++ ToonSpace/Models/ViewModels/ArtistPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToonSpace.Models.ViewModels
+{
+    public class ArtistPageViewModel
+    {
+        public List<ToonUser> Followers { get; set; }
+        public List<ToonUser> Following { get; set; }
+        public List<Upload> ArtistUploads { get; set; }
+        public List<Upload> TrendingUploads { get; set; }
+        public List<Upload> RecentUploads { get; set; }
+        public List<Upload> FeaturedUploads { get; set; }
+        public ToonUser Artist { get; set; }
+    }
+}
