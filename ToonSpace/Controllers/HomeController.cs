1fa43b5d8cf2b797d133cc402eff93c1daca6933
piaomu <driftwood.wahl@gmail.com>
2021-07-27 19:49:31 -0500

    Add ArtistPage

--- ToonSpace/Controllers/HomeController.cs
+++ ToonSpace/Controllers/HomeController.cs
@@ -58,19 +58,19 @@ namespace ToonSpace.Controllers
             return View(model);
         }
 
-        //[HttpGet]
-        //[Authorize]
-        //public async Task<IActionResult> ArtistPage()
-        //{
-        //    var artistId = await _uploadService.Get
-        //    ArtistPageViewModel model = new()
-        //    {
-        //        Followers = await _relationService.GetFollowersAsync(artistId),
-        //        Following = await _relationService.GetFollowingAsync(artistId),
-        //        TrendingUploads = await _uploadService.GetTrendingUploadsAsync()
-        //    };
-        //    return View(model);
-        //}
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> ArtistPage(string id)
+        {
+
+            ArtistPageViewModel model = new()
+            {
+                Followers = await _relationService.GetFollowersAsync(id),
+                Following = await _relationService.GetFollowingAsync(id),
+                TrendingUploads = await _uploadService.GetTrendingUploadsAsync()
+            };
+            return View(model);
+        }
         public IActionResult Privacy()
         {
             return View();
