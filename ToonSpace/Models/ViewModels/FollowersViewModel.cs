84635aaf24fe4448b78a5bf5a56e9804ce50dc31
piaomu <driftwood.wahl@gmail.com>
2021-09-08 16:52:04 -0500

    FollowersViewModel

---
+++ ToonSpace/Models/ViewModels/FollowersViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToonSpace.Models.ViewModels
+{
+    public class FollowersViewModel
+    {
+        public List<ToonUser> Followers { get; set; } 
+        public ToonUser Artist { get; set; }
+    }
+}
